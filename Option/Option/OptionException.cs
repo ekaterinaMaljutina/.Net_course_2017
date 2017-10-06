ae3abff5db525894a3509291a4cba8173711b5e9
ekaterinaMaljutina <e.maljutina@gmail.com>
2017-10-06 11:07:47 +0300

    impl option

---
+++ Option/Option/OptionException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyOption
+{
+    public class NotValueException : Exception
+    {
+        public NotValueException(string Message)
+            : base(Message)
+        {
+        }
+    }
+
+    public class NullValueException : Exception
+    {
+        public NullValueException(string Message)
+            : base(Message)
+        {
+        }
+    }
+
+}
+
